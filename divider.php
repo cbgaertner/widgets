e152db03c1454ad80a47592308cec4ff60bbf4a3
cbgaertner <97530665+cbgaertner@users.noreply.github.com>
2022-01-17 23:31:05 +0100

    Create divider.php

---
+++ divider.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<style>
+
+.divider {
+	width: 100%;
+	height: <?php echo htmlspecialchars($_GET["height"]); ?>;
+	background-color: <?php echo $_GET["color"]; ?>;
+}
+
+</style>
+</head>
+<body>
+
+<div class="divider"></div>
+
+</body>
+</html>
